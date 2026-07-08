136cad98d489a6410310a5f5570edc4bc0909906
Alireza Mohammadi <alireza1384928@gmail.com>
2026-07-08 12:35:29 +0330

    Functionality improvement

--- src/request.js
+++ src/request.js
@@ -3,10 +3,10 @@ import axios from "axios"
 const API_KEY = import.meta.env.VITE_API_KEY
 
 async function getWeatherData(city, days) {
-  const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+  const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric&cnt=${days * 8}`
   const response = await axios.get(URL)
 
-  return response.data.list.slice(0, 8 * days)
+  return response.data.list
 }
 
 export default getWeatherData
